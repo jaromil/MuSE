58a11148f53024c773f30eb38bd8a25d2a44badc
Luca Bigliardi <shammash@artha.org>
2009-10-03 12:56:56 +0100

    Import autogen.sh from FreeJ
    Signed-off-by: Luca Bigliardi <shammash@artha.org>

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,33 @@
 #!/bin/sh
 # Run this to set up the build system: configure, makefiles, etc.
 # (based on the version in enlightenment's cvs)
+#
 
 package="MuSE"
 
+olddir=`pwd`
+srcdir=`dirname $0`
+
+AUTOHEADER=autoheader
+if [ "`uname -s`" = "Darwin" ]; then
+  LIBTOOL=glibtool
+  LIBTOOLIZE=glibtoolize
+  ACLOCAL=aclocal
+  AUTOMAKE=automake
+else
+  LIBTOOL=libtool
+  LIBTOOLIZE=libtoolize
+  ACLOCAL=aclocal
+  AUTOMAKE=automake
+fi
+AUTOCONF=autoconf
+
+test -z "$srcdir" && srcdir=.
+
+cd "$srcdir"
 DIE=0
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+($AUTOCONF --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have autoconf installed to compile $package."
         echo "Download the appropriate package for your distribution,"
@@ -14,36 +35,42 @@ DIE=0
         DIE=1
 }
 
-(automake --version) < /dev/null > /dev/null 2>&1 || {
+($AUTOMAKE --version) < /dev/null > /dev/null 2>&1 || {
         echo
         echo "You must have automake installed to compile $package."
 	echo "Download the appropriate package for your system,"
 	echo "or get the source from one of the GNU ftp sites"
 	echo "listed in http://www.gnu.org/order/ftp.html"
-        DIE=1
+	DIE=1
+}
+
+($LIBTOOL --version) < /dev/null > /dev/null 2>&1 || {
+	echo
+	echo "You must have libtool installed to compile $package."
+	echo "Download the appropriate package for your system,"
+	echo "or get the source from one of the GNU ftp sites"
+	echo "listed in http://www.gnu.org/order/ftp.html"
+	DIE=1
 }
 
 if test "$DIE" -eq 1; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
 echo "Generating configuration files for $package, please wait...."
 
-echo "  aclocal -I m4"
-aclocal -I m4
+echo "  $ACLOCAL -I $srcdir/m4"
+$ACLOCAL -I $srcdir/m4 || exit -1
+echo "  $AUTOHEADER"
+$AUTOHEADER || exit -1
+echo "  $LIBTOOLIZE --automake -c"
+$LIBTOOLIZE --automake -c || exit -1
+echo "  $AUTOMAKE --add-missing -c"
+$AUTOMAKE --add-missing -c || exit -1
+echo "  $AUTOCONF"
+$AUTOCONF || exit -1
 
-echo "  autoheader"
-autoheader
+echo "Now you can run $srcdir/configure"
 
-echo "  automake --add-missing"
-automake --add-missing -c 
-
-echo "  autoconf"
-autoconf
-
+cd $olddir
 
